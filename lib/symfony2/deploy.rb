bc6f2220740ec9708fb753983e811e245f75382f
abadore <amy@badore.com>
2012-10-08 14:53:39 -0700

    removing try_sudo form deploy:share_childs

--- lib/symfony2/deploy.rb
+++ lib/symfony2/deploy.rb
@@ -64,9 +64,9 @@ namespace :deploy do
       pretty_print "--> Creating symlinks for shared directories"
 
       shared_children.each do |link|
-        run "#{try_sudo} mkdir -p #{shared_path}/#{link}"
-        run "#{try_sudo} sh -c 'if [ -d #{release_path}/#{link} ] ; then rm -rf #{release_path}/#{link}; fi'"
-        run "#{try_sudo} ln -nfs #{shared_path}/#{link} #{release_path}/#{link}"
+        run "mkdir -p #{shared_path}/#{link}"
+        run "sh -c 'if [ -d #{release_path}/#{link} ] ; then rm -rf #{release_path}/#{link}; fi'"
+        run "ln -nfs #{shared_path}/#{link} #{release_path}/#{link}"
       end
 
       puts_ok
@@ -77,9 +77,9 @@ namespace :deploy do
 
       shared_files.each do |link|
         link_dir = File.dirname("#{shared_path}/#{link}")
-        run "#{try_sudo} mkdir -p #{link_dir}"
-        run "#{try_sudo} touch #{shared_path}/#{link}"
-        run "#{try_sudo} ln -nfs #{shared_path}/#{link} #{release_path}/#{link}"
+        run "mkdir -p #{link_dir}"
+        run "touch #{shared_path}/#{link}"
+        run "ln -nfs #{shared_path}/#{link} #{release_path}/#{link}"
       end
 
       puts_ok
